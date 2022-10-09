505452dfafb4cebefa3b7032198d98191e819518
Noor Ali <nooralib180@gmail.com>
2022-10-09 14:11:23 +0500

    ...

---
+++ Small Projects/02) Carousel/script.js
@@ -0,0 +1,73 @@
+'use strict'
+
+const slides = document.querySelectorAll(".slide");
+const dotsContainer = document.querySelector(".dots");
+const nextBtn = document.querySelector(".btn--right");
+const prevBtn = document.querySelector(".btn--left");
+
+// slides.forEach((sl, i) => {
+//     sl.style.transform = `translateX(${(i) * 130}%)`;
+// })
+
+const slider = function(){
+    let curSlide = 0;
+    
+    // creating dots...
+    const createDots = function() {
+        
+        slides.forEach((_, i) => {
+            dotsContainer.insertAdjacentHTML('beforeend', `
+            <div class="dot" data-tab="${i}"></div>
+            `);
+        })
+    }
+    
+    // activating dots ...
+    const activateDot = function(){
+        const dots = document.querySelectorAll('.dot');
+        dots.forEach((dot) => {
+            dot.classList.remove('active');
+        });
+    
+        document.querySelector(`.dot[data-tab = "${curSlide}"]`).classList.add('active');
+    }
+    
+    // go to slide functionality...
+    const goToSlide = function(){
+        slides.forEach((slide, i) => {
+            slide.style.transform = `translateX(${130 * (i - curSlide)}%)`
+        });
+    }
+    
+    // initial conditions...
+    const init = function(){
+        createDots();
+        activateDot();
+        goToSlide();
+    }
+    init();
+    
+    // go to next Slide functionality...
+    const nextSlide = function(){
+        if(curSlide === slides.length - 1) curSlide = 0;
+        else curSlide++;
+    
+        goToSlide();
+        activateDot();
+    }
+    
+    // go to previous Slide functionality...
+    const prevSlide = function(){
+        if(curSlide === 0) curSlide = slides.length - 1;
+        else curSlide--;
+    
+        goToSlide();
+        activateDot();
+    }
+    
+    // handling eventlisteners...
+    nextBtn.addEventListener('click', nextSlide);
+    prevBtn.addEventListener('click', prevSlide);
+}
+
+slider();

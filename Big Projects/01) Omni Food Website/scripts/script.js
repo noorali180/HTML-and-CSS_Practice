fd458bc4b9027a94cbd7f62f739a109dba690502
Noor Ali <nooralib180@gmail.com>
2022-11-19 12:01:37 +0500

    ...

--- Big Projects/01) Omni Food Website/scripts/script.js
+++ Big Projects/01) Omni Food Website/scripts/script.js
@@ -3,21 +3,58 @@
 const header = document.querySelector('.header');
 const navOpenBtn = document.querySelector('.mobile-nav-icon[name = "menu-outline"]');
 const navCloseBtn = document.querySelector('.mobile-nav-icon[name = "close-outline"]');
-const navLinks = document.querySelectorAll('.main-nav-link');
 
 navOpenBtn.addEventListener('click', function(e){
     e.preventDefault();
-    header.classList.add('nav-open');
+    header.classList.toggle('nav-open');
 });
 
 navCloseBtn.addEventListener('click', function(e){
     e.preventDefault();
-    header.classList.remove('nav-open');
+    header.classList.toggle('nav-open');
+});
+
+// smooth scrolling...
+const navLinks = document.querySelectorAll('a:link');
+
+navLinks.forEach((link) => link.addEventListener('click', function(e){
+    e.preventDefault();
+
+    
+    if( e.target.classList.contains('main-nav-link')){
+        header.classList.remove('nav-open');
+    }
+
+    const link = e.target.getAttribute('href');
+
+    if(link === null){
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    }
+    else if(link !== '#' && link.startsWith('#')){
+        const sectionEl = document.querySelector(link);
+        sectionEl.scrollIntoView({behavior: 'smooth'});
+    }
+}));
+
+// Sticky Header...
+
+const heroEl = document.querySelector('.section-hero');
+
+const observer = new IntersectionObserver(function(entries){
+    
+        const [entry] = entries;
+
+        if(!entry.isIntersecting) document.body.classList.add('sticky');
+        else document.body.classList.remove('sticky');
+    
+},
+{
+    root: null,
+    threshold: 0,
+    rootMargin: '-80px',
 })
 
-navLinks.forEach((link) => {
-    link.addEventListener('click', function(e){
-        e.preventDefault();
-        header.classList.remove('nav-open');
-    })
-})
+observer.observe(heroEl);
